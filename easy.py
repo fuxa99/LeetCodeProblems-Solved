57c0b01b3a8968aac912750c96756db2a87b438a
Dominik Fuxa <87151953+fuxa99@users.noreply.github.com>
2022-08-25 10:21:21 +0000

    Deleted comments

--- easy.py
+++ easy.py
@@ -1,19 +1,10 @@
 def twoSum(nums, target):
-  """
-  :type nums: List[int]
-  :type target: int
-  :rtype: List[int]
-  """
   for i in range(len(nums)):
     if (target - nums[i]) in nums: 
       if i != nums.index(target - nums[i]):
         return [i, nums.index(target - nums[i])]
 
 def isPowerOfThree(n: int) -> bool:
-  """
-  :type n: int
-  :rtype: bool
-  """
   if n <= 0:
     return False
   num = n
